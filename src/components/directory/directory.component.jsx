1564ed0ad2e8171c4c5bc24a033ace453ce8fc5b
youngsal <waleola805@gmail.com>
2025-01-28 12:10:59 +0100

    created project with create react app and add a files for homepage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -77,22 +77,6 @@ export class Directory extends React.Component {
                 size: 'large'
             },
             {
-                title: 'jackets',
-                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id: 2
-            },
-            {
-                title: 'sneakers',
-                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id: 3
-            },
-            {
-                title: 'women',
-                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                id: 4,
-                size: 'large'
-            },
-            {
                 title: 'men',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 id: 5,
