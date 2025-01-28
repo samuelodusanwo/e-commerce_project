041077f8fd860dff031777eeb6b12ba61d172778
youngsal <waleola805@gmail.com>
2025-01-28 17:06:37 +0100

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import {HomePage} from './page/homepage/homepage.component';
 import './App.scss';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hello Hats</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
