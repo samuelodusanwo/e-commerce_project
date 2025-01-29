5b782772f953e19ec2bd666cf4254ab46ede94b7
youngsal <waleola805@gmail.com>
2025-01-29 17:13:49 +0100

    update our menu-items to navigate with useNavigate HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -33,6 +33,63 @@ export class Directory extends React.Component {
                 size: 'large'
             },
             {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: ''
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: ''
+            },
+            {
+                title: 'women',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                id: 4,
+                linkUrl: '',
+                size: 'large'
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: ''
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: ''
+            },
+            {
+                title: 'women',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                id: 4,
+                linkUrl: '',
+                size: 'large'
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: ''
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: ''
+            },
+            {
+                title: 'women',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                id: 4,
+                linkUrl: '',
+                size: 'large'
+            },
+            {
                 title: 'men',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 id: 5,
