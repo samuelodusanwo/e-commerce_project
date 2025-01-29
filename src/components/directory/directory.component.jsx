92c1faf66974d3a99e1863ac17dba87a71005e53
youngsal <waleola805@gmail.com>
2025-01-29 17:10:27 +0100

    update our menu-items to navigate with useNavigate HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,27 +11,32 @@ export class Directory extends React.Component {
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
+                linkUrl: '/hats'
             },
             {
                 title: 'jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },
             {
                 title: 'sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id: 3
+                id: 3,
+                linkUrl: ''
             },
             {
                 title: 'women',
                 imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                 id: 4,
+                linkUrl: '',
                 size: 'large'
             },
             {
                 title: 'men',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 id: 5,
+                linkUrl: '',
                 size: 'large'
             }]
         }
@@ -41,8 +46,8 @@ export class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />    
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps} />    
                     ))
                 }
             </div>
