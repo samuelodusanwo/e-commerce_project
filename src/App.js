374c120466744d9e8f7d84f356eef70f5cf8dd9d
youngsal <waleola805@gmail.com>
2025-02-02 14:14:32 +0100

    added shop page and collection page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route} from 'react-router-dom';
 import { HomePage } from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
+import Header from './components/header/header.component';
 import './App.scss';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
