e477ebec60af7ee5e24c71f8495abf954d40c43f
youngsal <waleola805@gmail.com>
2025-02-02 14:15:12 +0100

    added shop page and collection page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,6 +12,27 @@ const Header = () => (
             <Link className='option' to='/shop'>Shop</Link>
             <Link className='option' to='/contact'>Contact</Link>
             <Link className='option' to='/SignIn'>SignIn</Link>
+            <Link className='option' to='/shop'>Shop</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/SignIn'>SignIn</Link>
+            <Link className='option' to='/shop'>Shop</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/SignIn'>SignIn</Link>
+            <Link className='option' to='/shop'>Shop</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/SignIn'>SignIn</Link>
+            <Link className='option' to='/shop'>Shop</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/SignIn'>SignIn</Link>
+            <Link className='option' to='/shop'>Shop</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/SignIn'>SignIn</Link>
+            <Link className='option' to='/shop'>Shop</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/SignIn'>SignIn</Link>
+            <Link className='option' to='/shop'>Shop</Link>
+            <Link className='option' to='/contact'>Contact</Link>
+            <Link className='option' to='/SignIn'>SignIn</Link>
         </div>
     </div>
 )
