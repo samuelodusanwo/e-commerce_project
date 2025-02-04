d8e603403b9a2a7753fdb42d3fcec02047bab72c
youngsal <waleola805@gmail.com>
2025-02-04 16:41:16 +0100

    created custom-button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -27,7 +28,7 @@ class SignIn extends React.Component {
     render() {
         return( 
             <div className="sign-in">
-                <h1>I already have an account</h1>
+                <h1 className="title">I already have an account</h1>
                 <span>Sign in with your email and password</span>
                 <form action="#" onSubmit={this.handleSubmit}>
                     <FormInput
@@ -48,7 +49,9 @@ class SignIn extends React.Component {
                         required 
                     />
 
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit">
+                        SIGN IN 
+                    </CustomButton>
                 </form>
             </div>
         )
