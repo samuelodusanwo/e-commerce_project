ddbd1eef30c18884944b1c319f8c60ab2ef92214
youngsal <waleola805@gmail.com>
2025-02-04 08:01:06 +0100

    adding functionality to my form inputs

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -29,11 +30,25 @@ class SignIn extends React.Component {
                 <h1>I already have an account</h1>
                 <span>Sign in with your email and password</span>
                 <form action="#" onSubmit={this.handleSubmit}>
-                    <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-                    <label>Email</label> 
+                    <input 
+                        type="email" 
+                        name="email" 
+                        value={this.state.email} 
+                        handleChange={this.handleChange} 
+                        label
+                        required 
+                    />
+                    {/* <label>Email</label>  */}
 
-                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-                    <label>Password</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        value={this.state.password} 
+                        handleChange={this.handleChange} 
+                        label
+                        required 
+                    />
+                    {/* <label>Password</label> */}
 
                     <input type="submit" value="Submit Form" />
                 </form>
