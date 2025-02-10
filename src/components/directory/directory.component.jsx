67cc44bcd0d76371d94c2ffd4a0e66a8eedc0d80
youngsal <waleola805@gmail.com>
2025-02-10 16:21:20 +0100

    initial commit: menu-state update

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -39,6 +39,7 @@ class Directory extends React.Component {
                 linkUrl: '',
                 size: 'large'
             }]
+            sections: [{
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
@@ -70,6 +71,103 @@ class Directory extends React.Component {
                 linkUrl: '',
                 size: 'large'
             }]
+            sections: [{
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1,
+                linkUrl: '/hats'
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: ''
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: ''
+            },
+            {
+                title: 'women',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                id: 4,
+                linkUrl: '',
+                size: 'large'
+            },
+            {
+                title: 'men',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                id: 5,
+                linkUrl: '',
+                size: 'large'
+            }]
+            sections: [{
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1,
+                linkUrl: '/hats'
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: ''
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: ''
+            },
+            {
+                title: 'women',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                id: 4,
+                linkUrl: '',
+                size: 'large'
+            },
+            {
+                title: 'men',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                id: 5,
+                linkUrl: '',
+                size: 'large'
+            }]
+            sections: [{
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1,
+                linkUrl: '/hats'
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: ''
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: ''
+            },
+            {
+                title: 'women',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                id: 4,
+                linkUrl: '',
+                size: 'large'
+            },
+            {
+                title: 'men',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                id: 5,
+                linkUrl: '',
+                size: 'large'
+            }]
+            sections: [{
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
