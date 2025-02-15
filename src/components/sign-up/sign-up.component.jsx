286246d1969ac3ba0db83fb6519f7efe8a26cae8
youngsal <waleola805@gmail.com>
2025-02-15 03:07:33 +0100

    creating of sign up component and it's function

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -47,9 +47,7 @@ class SignUp extends React.Component{
     handleChange = (event) => {
         const { name, value } = event.target;
 
-        this.setState({[name]: value})
-
-        console.log(this.state);
+        this.setState({[name]: value}, () => console.log(this.state));
     }
 
     render (){
