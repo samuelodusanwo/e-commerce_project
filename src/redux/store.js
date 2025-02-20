3557a3d6e48ea45a5fcc11b5fa415e012570df11
youngsal <waleola805@gmail.com>
2025-02-20 10:58:15 +0100

    configuration of redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './root-reducer';
 import logger from 'redux-logger';
+import rootReducer from './root-reducer';
 
-const middleware = [logger];
+const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
