8c1e8a26a4a636617675d26daa9646cd55f3cbe5
youngsal <waleola805@gmail.com>
2025-02-22 00:43:26 +0100

    added redux, and implimented userReducer and userAction to header and app component

--- src/redux/users/user.action.js
+++ src/redux/users/user.action.js
@@ -2,7 +2,3 @@ export const setCurrentUser = user => ({
     type: 'SET_CURRENT_USER',
     payload: user
 })
-// export const setCurrentUser = user => ({
-//     type: 'SET_CURRENT_USER',
-//     payload: user
-// })
