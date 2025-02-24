1c1d6b97f4dadf11f5184f4221579e55e99d438e
youngsal <waleola805@gmail.com>
2025-02-24 13:34:44 +0100

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUsers
 })
 
 const mapDispatchToProps = (dispatch) => ({
