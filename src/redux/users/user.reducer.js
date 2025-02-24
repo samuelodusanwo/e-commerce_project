3fd1bf128f7a8bcab60e54fe14483971b73156b0
youngsal <waleola805@gmail.com>
2025-02-24 22:34:01 +0100

    Added cart components and updated user reducer

--- src/redux/users/user.reducer.js
+++ src/redux/users/user.reducer.js
@@ -17,22 +17,3 @@ const userReducer = (state = INITIAL_STATE, action) => {
 }
 
 export default userReducer;
-import { userActionTypes } from './user.types';
-
-const INITIAL_STATE = {
-    currentUser: null
-}
-
-const userReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type){
-        case userActionTypes.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: action.payload
-            }
-        default:
-            return state
-    }
-}
-
-export default userReducer;
