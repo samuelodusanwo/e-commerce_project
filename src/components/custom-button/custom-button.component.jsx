7916b0b0879251b29bc8e4cffebe032a65bc86a7
youngsal <waleola805@gmail.com>
2025-04-05 14:56:54 +0100

    initial push

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -19,4 +19,26 @@ const CustomButton = ({children, onClick, isGoogleSignIn, ...otherProps}) => (
     </button>
 )
 
+
+import React from "react";
+
+import './custom-button.styles.scss';
+
+const CustomButton = ({children, onClick, isGoogleSignIn, ...otherProps}) => (
+    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} onClick={onClick} {...otherProps}>
+        {children}
+    </button>
+)
+
 export default CustomButton;
+import React from "react";
+
+import './custom-button.styles.scss';
+
+const CustomButton = ({children, onClick, isGoogleSignIn, ...otherProps}) => (
+    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} onClick={onClick} {...otherProps}>
+        {children}
+    </button>
+)
+
+
