e4bf6d26ed51b2316b709ce255042965f52d88f8
youngsal <waleola805@gmail.com>
2025-04-16 17:47:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -500,5 +500,3 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
