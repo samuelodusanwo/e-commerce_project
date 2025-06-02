09083adfcdf5177df33a244ee3a309c5dc5a6204
youngsal <waleola805@gmail.com>
2025-06-02 23:32:54 +0100

    initial commit

--- src/components/collection/collection-item/collection-item.component.jsx
+++ src/components/collection/collection-item/collection-item.component.jsx
@@ -198,23 +198,3 @@ const CollectionItem = ({id, name, imageUrl, price}) => (
 )
 
 export default CollectionItem;
-import React from 'react';
-
-import './collection-item.styles.scss';
-
-const CollectionItem = ({id, name, imageUrl, price}) => (
-    <div className='collection-item' key={id}>
-        <div
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className="price">{price}</span>
-        </div>
-    </div>
-)
-
-export default CollectionItem;
