f0273bff593871396ec8445da26159a7881a290e
youngsal <waleola805@gmail.com>
2025-06-21 10:10:02 +0100

    initial commit

--- src/components/collection/collection-item/collection-item.component.jsx
+++ src/components/collection/collection-item/collection-item.component.jsx
@@ -466,3 +466,111 @@ const CollectionItem = ({id, name, imageUrl, price}) => (
         </div>
     </div>
 )
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, imageUrl, price}) => (
+    <div className='collection-item' key={id}>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+)
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, imageUrl, price}) => (
+    <div className='collection-item' key={id}>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+)
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, imageUrl, price}) => (
+    <div className='collection-item' key={id}>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+)
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, imageUrl, price}) => (
+    <div className='collection-item' key={id}>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+)
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, imageUrl, price}) => (
+    <div className='collection-item' key={id}>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+)
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+const CollectionItem = ({id, name, imageUrl, price}) => (
+    <div className='collection-item' key={id}>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className="price">{price}</span>
+        </div>
+    </div>
+)
