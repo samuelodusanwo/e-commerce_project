53117f0e87045d3ccbf83ed0f92c575fe7810515
youngsal <waleola805@gmail.com>
2025-10-11 04:58:17 +0100

    initial commit

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -806,5 +806,3 @@ const CartIcon = ({toggleCartHidden}) => (
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-
-export default connect(null, mapDispatchToProps)(CartIcon
