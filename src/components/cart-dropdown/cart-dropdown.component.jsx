5422f035172ead29ae8725ea382ea6c59a012cf1
Samuel Odusanwo <odusanwosamuel6@gmail.com>
2026-06-18 03:42:08 +0100

    initial push

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -58,15 +58,3 @@ const CartDropdown = () => (
 )
 
 export default CartDropdown;
-import React from "react";
-import CustomButton from "../custom-button/custom-button.component";
-import './cart-dropdown.styles.scss';
-
-const CartDropdown = () => (
-    <div className="cart-dropdown">
-        <div className="cart-items" />
-        <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
-)
-
-export default CartDropdown;
